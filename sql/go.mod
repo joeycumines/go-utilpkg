0425994ce163843090b9ee5fa3f9fb81108f392d
Joseph Cumines <joeycumines@gmail.com>
2022-06-26 11:04:09 +1000

    Add log package using logrus-style logger
    fbshipit-source-id: f83f607

--- sql/go.mod
+++ sql/go.mod
@@ -1,3 +1,7 @@
 module github.com/joeycumines/go-sql
 
 go 1.18
+
+require github.com/sirupsen/logrus v1.8.1
+
+require golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
---
+++ sql/log/doc.go
@@ -0,0 +1,3 @@
+// Package log implements a standardised logger interface, for use with this module.
+// This module uses github.com/sirupsen/logrus, though it can be adapted to any equivalent implementation.
+package log
