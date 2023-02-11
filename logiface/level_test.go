49637a1d96033ffc2ea098c12c94a5dbe919c9c8
Joseph Cumines <joeycumines@gmail.com>
2023-02-11 15:52:31 +1000

    Add tests for logiface.Level methods Custom and Syslog

--- logiface/level_test.go
+++ logiface/level_test.go
@@ -39,3 +39,73 @@ func ExampleLevel_Enabled() {
 	//"9" (9): true
 	//"127" (127): true
 }
+
+func ExampleLevel_Custom() {
+	p := func(level Level) {
+		fmt.Printf("%q (%d): %v\n", level, level, level.Custom())
+	}
+	p(math.MinInt8)
+	p(-2)
+	p(LevelDisabled)
+	p(LevelEmergency)
+	p(LevelAlert)
+	p(LevelCritical)
+	p(LevelError)
+	p(LevelWarning)
+	p(LevelNotice)
+	p(LevelInformational)
+	p(LevelDebug)
+	p(LevelTrace)
+	p(9)
+	p(math.MaxInt8)
+	//output:
+	//"-128" (-128): false
+	//"-2" (-2): false
+	//"disabled" (-1): false
+	//"emerg" (0): false
+	//"alert" (1): false
+	//"crit" (2): false
+	//"err" (3): false
+	//"warning" (4): false
+	//"notice" (5): false
+	//"info" (6): false
+	//"debug" (7): false
+	//"trace" (8): false
+	//"9" (9): true
+	//"127" (127): true
+}
+
+func ExampleLevel_Syslog() {
+	p := func(level Level) {
+		fmt.Printf("%q (%d): %v\n", level, level, level.Syslog())
+	}
+	p(math.MinInt8)
+	p(-2)
+	p(LevelDisabled)
+	p(LevelEmergency)
+	p(LevelAlert)
+	p(LevelCritical)
+	p(LevelError)
+	p(LevelWarning)
+	p(LevelNotice)
+	p(LevelInformational)
+	p(LevelDebug)
+	p(LevelTrace)
+	p(9)
+	p(math.MaxInt8)
+	//output:
+	//"-128" (-128): false
+	//"-2" (-2): false
+	//"disabled" (-1): false
+	//"emerg" (0): true
+	//"alert" (1): true
+	//"crit" (2): true
+	//"err" (3): true
+	//"warning" (4): true
+	//"notice" (5): true
+	//"info" (6): true
+	//"debug" (7): true
+	//"trace" (8): false
+	//"9" (9): false
+	//"127" (127): false
+}
