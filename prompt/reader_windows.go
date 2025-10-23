67364eb904233c06b31798c6f9fbc0ebd1e9dab8
Joseph Cumines <joeycumines@gmail.com>
2025-10-24 08:34:47 +1000

    prompt: Improve robustness of the windows reader impl

--- prompt/reader_windows.go
+++ prompt/reader_windows.go
@@ -4,6 +4,7 @@ package prompt
 
 import (
 	"errors"
+	"sync"
 	"syscall"
 	"unicode/utf8"
 	"unsafe"
@@ -17,28 +18,53 @@ var procGetNumberOfConsoleInputEvents = kernel32.NewProc("GetNumberOfConsoleInpu
 
 // WindowsReader is a Reader implementation for Win32 console.
 type WindowsReader struct {
-	tty *tty.TTY
+	mu   sync.RWMutex
+	tty  *tty.TTY
+	open bool
+	err  error // close error
 }
 
 // Open should be called before starting input
 func (p *WindowsReader) Open() error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	t, err := tty.Open()
 	if err != nil {
 		return err
 	}
 	p.tty = t
+	p.open = true
 	return nil
 }
 
 // Close should be called after stopping input
 func (p *WindowsReader) Close() error {
-	return p.tty.Close()
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if !p.open {
+		// return any previous close error
+		return p.err
+	}
+	// N.B. This error will be visible ONLY if tty.Close() panics (for subsequent calls to Close).
+	p.err = errors.New("panic during reader close")
+	p.open = false
+	p.err = tty.Close()
+	return p.err
+}
+
+func (p *WindowsReader) getTTY() *tty.TTY {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	// WARNING: deliberately doesn't guard on open state
+	return p.tty
 }
 
 // Read returns byte array.
 func (p *WindowsReader) Read(buff []byte) (int, error) {
+	tty := p.getTTY()
+
 	var ev uint32
-	r0, _, err := procGetNumberOfConsoleInputEvents.Call(p.tty.Input().Fd(), uintptr(unsafe.Pointer(&ev)))
+	r0, _, err := procGetNumberOfConsoleInputEvents.Call(tty.Input().Fd(), uintptr(unsafe.Pointer(&ev)))
 	if r0 == 0 {
 		return 0, err
 	}
@@ -46,14 +72,14 @@ func (p *WindowsReader) Read(buff []byte) (int, error) {
 		return 0, errors.New("EAGAIN")
 	}
 
-	r, err := p.tty.ReadRune()
+	r, err := tty.ReadRune()
 	if err != nil {
 		return 0, err
 	}
 
 	n := utf8.EncodeRune(buff[:], r)
-	for p.tty.Buffered() && n < len(buff) {
-		r, err := p.tty.ReadRune()
+	for tty.Buffered() && n < len(buff) {
+		r, err := tty.ReadRune()
 		if err != nil {
 			break
 		}
@@ -64,7 +90,7 @@ func (p *WindowsReader) Read(buff []byte) (int, error) {
 
 // GetWinSize returns WinSize object to represent width and height of terminal.
 func (p *WindowsReader) GetWinSize() *WinSize {
-	w, h, err := p.tty.Size()
+	w, h, err := p.getTTY().Size()
 	if err != nil {
 		panic(err)
 	}
