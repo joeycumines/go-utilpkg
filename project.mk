c1c2b84487f72f5ebacddf743422266cd81af7b0
Joseph Cumines <joeycumines@gmail.com>
2026-02-08 21:36:16 +1000

    Fixes

--- project.mk
+++ project.mk
@@ -19,8 +19,7 @@ GRIT_DST ?= \
     sql$(MAP_SEPARATOR)https://github.com/joeycumines/go-sql.git \
     prompt$(MAP_SEPARATOR)https://github.com/joeycumines/go-prompt.git \
     grpc-proxy$(MAP_SEPARATOR)https://github.com/joeycumines/grpc-proxy.git \
-    floater$(MAP_SEPARATOR)https://github.com/joeycumines/floater.git \
-    goja-eventloop$(MAP_SEPARATOR)https://github.com/joeycumines/goja-eventloop.git
+    floater$(MAP_SEPARATOR)https://github.com/joeycumines/floater.git
 # N.B. relative to the go module it applies to
 DEADCODE_IGNORE_PATTERNS_FILE = .deadcodeignore
 DEADCODE_ERROR_ON_UNIGNORED = true
@@ -28,31 +27,3 @@ DEADCODE_ERROR_ON_UNIGNORED = true
 .PHONY: betteralign-apply
 betteralign-apply:
 	$(MAKE) betteralign BETTERALIGN_FLAGS=-apply
-
-# Run specific promise regression test 5 times
-.PHONY: test-promise-race-concurrent
-test-promise-race-concurrent:
-	cd $(PROJECT_ROOT)/eventloop && go test -v -run TestPromiseRace_ConcurrentThenReject_HandlersCalled -count=5 2>&1 | tail -100
-
-# Race detector for eventloop
-.PHONY: race-eventloop
-race-eventloop:
-	cd $(PROJECT_ROOT) && go test -race -timeout=5m ./eventloop/... 2>&1 | tail -50
-
-# Race detector for eventloop - detailed failures
-.PHONY: race-eventloop-failures
-race-eventloop-failures:
-	cd $(PROJECT_ROOT) && go test -race -timeout=5m ./eventloop/... 2>&1 | tee /tmp/race_test.log | grep -E "(^=== RUN|---.*FAIL|race detected|DATA RACE|^FAIL|^panic|panic:|Warning)" | head -100; echo "Exit: $$?"
-
-# Race test with full output
-.PHONY: race-eventloop-full
-race-eventloop-full:
-	cd $(PROJECT_ROOT) && go test -race -timeout=6m ./eventloop/... 2>&1 | tee /tmp/race_test.log; echo "Test exit: $$?"
-
-# EXPAND-049: Cleanup leftover *_fixed.go files
-.PHONY: cleanup-fixed-files
-cleanup-fixed-files:
-	rm -f $(PROJECT_ROOT)/goja-eventloop/base64_test_fixed.go
-	rm -f $(PROJECT_ROOT)/goja-eventloop/crypto_test_fixed.go
-	rm -f $(PROJECT_ROOT)/goja-eventloop/nexttick_test_fixed.go
-	@echo "Cleaned up *_fixed.go files"
