ae2e00566b7d7779d8df012fc7c5d81839d32268
Joseph Cumines <joeycumines@gmail.com>
2026-08-21 09:44:04 +1000

    Update goroutine ID fast path for Go 1.27
    fbshipit-source-id: 5cad2da

--- supported.go
+++ supported.go
@@ -1,4 +1,4 @@
-//go:build (go1.26 && !go1.27 && amd64 && !js && !tinygo && gc) || (go1.26 && !go1.27 && arm64 && !js && !tinygo && gc)
+//go:build ((go1.26 || go1.27) && !go1.28 && amd64 && !js && !tinygo && gc) || ((go1.26 || go1.27) && !go1.28 && arm64 && !js && !tinygo && gc)
 
 // NOTICE: This build constraint is logically "fast_amd64.s OR fast_arm64.s".
 
